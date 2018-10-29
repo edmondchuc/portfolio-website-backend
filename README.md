45ff208aa97b05060dd3aca191aeed97a501365a
Edmond Chuc <edmond.chuc@outlook.com>
2018-10-29 19:52:00 +1000

    Added route to send email request to sendgrid

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Installation
 Install the modules in `requirements.txt` and obtain a valid API key from [SendGrid](https://sendgrid.com).
 
-Set environment variable `SENDGRID_API_KEY` to the API key obtained from your SendGrid account.
+Set environment variable `SENDGRID_API_KEY` to the API key obtained from your SendGrid account. Manually do this if you're using PyCharm in the `Edit Configurations` menu of the project.
+
+Ensure that an `email_to.txt` file exists containing the email address receiving the messages from the contact form.
+The application will read from this file and set `EMAIL_TO` as the email address destination.
 
 Run the application using `python app.py`.
 
--- app.py
+++ app.py
@@ -1,22 +1,57 @@
 from flask import Flask, render_template, request
 
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import sendgrid
+import os
+from sendgrid.helpers.mail import *
+
+sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+
 app = Flask(__name__)
+
+# read the email address from file
+EMAIL_TO = None
+with open('email_to.txt', 'r') as myfile:
+    EMAIL_TO=myfile.read().replace('\n', '')
+    EMAIL_TO=Email(EMAIL_TO)
 
 
 @app.route('/contact', methods=['POST'])
 def contact():
     first_name = request.form.get('first_name')
     last_name = request.form.get('last_name')
-    email = request.form.get('email')
+    email_from = request.form.get('email')
     phone = request.form.get('phone')
     message = request.form.get('message')
-    print(
-        f'first name: {first_name}\n'
-        f'last name: {last_name}\n'
-        f'email: {email}\n'
-        f'phone: {phone}\n'
-        f'message: {message}'
+
+    subject = f'[www.edmondchuc.com] New message from {first_name}'
+
+    content = Content('text/plain', f"""
+    New message received from www.edmondchuc.com
+    
+    Sender's details:    
+    Name: {first_name} {last_name}
+    Email: {email_from}
+    Phone: {phone}
+    
+    Message: 
+    {message}
+    """)
+
+    mail = Mail(
+        Email('notification@edmondchuc.com'), # the sender email
+        subject,
+        EMAIL_TO,
+        content
     )
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(f'New email sent.\n'
+          f'Status code: {response.status_code}'
+          f'Body: {response.body}'
+          f'Headers: {response.headers}')
+
+    print(content.value)
     return 'Thank you for your message.'
 
 
@@ -24,6 +59,19 @@ def contact():
 def index():
     return 'Hello, World!'
 
+
+@app.route('/test', methods=['POST'])
+def test():
+    from_email = Email("edmond.chuc@edmondchuc.com")
+    to_email = Email("edmond.chuc@outlook.com")
+    subject = "[www.edmondchuc.com] Sending with SendGrid is Fun"
+    content = Content("text/plain", "and easy to do anywhere, even with Python")
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+    return 'test completed'
 
 if __name__ == '__main__':
     app.run(debug=True)
