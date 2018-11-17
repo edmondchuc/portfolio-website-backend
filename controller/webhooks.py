fab99f982cb7b7b1ed79125e6ae71af6bf3e2c2b
Edmond Chuc <edmond.chuc@outlook.com>
2018-11-18 01:22:11 +1000

    Added bash script to webhook for portfolio website

--- controller/webhooks.py
+++ controller/webhooks.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, request
+import subprocess
 
+from flask import Blueprint, request
 from github_webhook import Webhook
 
 webhook_blueprint = Blueprint('webhooks', __name__)
@@ -9,4 +10,7 @@ webhook = Webhook(webhook_blueprint) # register GitHub webhook and define '/post
 @webhook.hook() # defines a handler for the 'push' event.
 def on_portfolio_website_push(data):
     full_name = data['repository']['full_name']
-    print(f'Received push with: {full_name}')
+    print(f'Received push from: {full_name}')
+    if full_name == 'edmondchuc/portfolio-website':
+        subprocess.call('./github_webhook_bash_scripts/portfolio_asset_mv.sh')
+        
---
+++ github_webhook_bash_scripts/portfolio_asset_mv.sh
@@ -0,0 +1,35 @@
+msg='webhook-api'
+
+echo $msg: Changing directory to ~
+cd ~
+
+if [ ! -d "projects" ]; then
+        echo $msg: Creating directory ~/projects
+        mkdir ~/projects
+fi
+
+echo $msg: Changing directoy to ~/projects.
+cd ~/projects
+
+if [ ! -d "portfolio-website" ]; then
+        echo $msg: Cloning from https://github.com/edmondchuc/portfolio-website.git
+        git clone https://github.com/edmondchuc/portfolio-website.git
+fi
+
+echo $msg: Changing directory to portfolio-website
+cd portfolio-website
+
+echo $msg: Pulling from https://github.com/edmondchuc/portfolio-website.git
+git pull
+
+# Note: this expects the destination directory already exists. Otherwise, manually create using sudo.
+echo $msg: Copying index.html to /var/www/edmondchuc/html
+cp index.html /var/www/edmondchuc.com/html
+
+echo $msg: Copying static/css to /var/www/edmondchuc.com/html/static/css
+cp static/css/* /var/www/edmondchuc.com/html/static/css
+
+echo $msg: Copying static/img to /var/www/edmondchuc.com/html/static/img
+cp static/img/* /var/www/edmondchuc.com/html/static/img
+
+echo $msg: Successfully finished webhook event 'for' edmondchuc/portfolio-website.
