06194e46d1fe182ea8a442178a2eba057d3ec098
Edmond Chuc <edmond.chuc@outlook.com>
2018-11-20 18:05:05 +1000

    Added webhook and script for blog site

--- controller/webhooks.py
+++ controller/webhooks.py
@@ -1,4 +1,5 @@
 import subprocess
+import os
 
 from flask import Blueprint, request
 from github_webhook import Webhook
@@ -6,10 +7,49 @@ from github_webhook import Webhook
 webhook_blueprint = Blueprint('webhooks', __name__)
 webhook = Webhook(webhook_blueprint) # register GitHub webhook and define '/postreceive' endpoint
 
+# Constants
+GITHUB_WEBHOOK_BASH_SCRIPTS_PATH = './github_webhook_bash_scripts'
+
+
+def get_full_name(data):
+    """
+    Return the full_name of the GitHub data object.
+
+    :param data: GitHub data object as JSON.
+    :type data: JSON
+    :return: str
+    :rtype: str
+    """
+    return data['repository']['full_name']
+
 
 @webhook.hook() # defines a handler for the 'push' event.
 def on_portfolio_website_push(data):
-    full_name = data['repository']['full_name']
+    """
+    This route is called when GitHub provides a push from my portfolio-website repository.
+
+    :param data: GitHub data object as JSON.
+    :type data: JSON
+    :return: None
+    :rtype: None
+    """
+    full_name = get_full_name(data)
     print(f'Received push from: {full_name}')
     if full_name == 'edmondchuc/portfolio-website':
-        subprocess.call('./github_webhook_bash_scripts/portfolio_asset_mv.sh')
+        subprocess.call(os.path.join(GITHUB_WEBHOOK_BASH_SCRIPTS_PATH, 'portfolio_asset_mv.sh'))
+
+
+@webhook.hook()
+def on_blog_site_push(data):
+    """
+    This route is called when GitHub provides a push from my blog-site repository.
+
+    :param data: GitHub data object as JSON.
+    :type data: JSON
+    :return: None
+    :rtype: None
+    """
+    full_name = get_full_name(data)
+    print(f'Received push from: {full_name}')
+    if full_name == 'edmondchuc/blog-site':
+        subprocess.call(os.path.join(GITHUB_WEBHOOK_BASH_SCRIPTS_PATH, 'build-blog-and-deploy.sh'))
---
+++ github_webhook_bash_scripts/build-blog-and-deploy.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+msg='webhook-api'
+
+echo $msg: Changing directory to ~
+cd ~
+
+if [ ! -d "projects" ]; then
+        echo $msg: Creating directory ~/projects
+        mkdir ~/projects
+fi
+
+echo $msg: Changing directoy to ~/projects.
+cd ~/projects
+
+echo $msg: Checking if blog-site directory exists
+if [ ! -d "blog-site" ]; then
+	echo $msg: Cloning git repository for blog-site
+	git clone https://github.com/edmondchuc/blog-site.git
+fi
+
+echo $msg: Changing directoy to blog-site
+cd blog-site/
+
+echo $msg: Pulling from git repository for updates
+git pull
+
+echo $msg: Install pelican via pip3
+pip3 install pelican
+
+#TODO: check if this command results in any error. there should be a
+#	command to check the previous application's exit code
+#TODO: stop the script if it does not build correctly and exit.
+echo $msg: Build the content with 'pelican content' command
+pelican content
+
+echo $msg: Check if ~/var/www/edmondchuc.com/html/blog/ exists
+if [ ! -d "~/var/www/edmondchuc.com/html/blog/" ]; then
+	echo $msg: ERROR! Directory does not exist. Please create it manually with sudo and assign permissions correctly to the user.
+	exit 1
+fi
+
+echo $msg: Removing existing files in ~/var/www/edmondchuc.com/html/blog/
+rm -r ~/var/www/edmondchuc.com/html/blog/*
+
+echo $msg: Copying output of pelican to ~/var/www/edmondchuc.com/html/blog/
+cp -r output/* ~/var/www/edmondchuc.com/html/blog
+
+echo Built and deployed successfully
